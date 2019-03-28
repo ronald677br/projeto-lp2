016c7bf980b9442a8b595ba4109eb37d7cf77506
Ronaldo Manfredini <ronald677br@gmail.com>
2019-03-27 22:02:34 -0300

    funcionario, disciplina

--- src/models/Disciplina.java
+++ src/models/Disciplina.java
@@ -21,4 +21,39 @@ public class Disciplina {
 		out += " \nDuração: " + this.duracao;
 		System.out.println(out);
 	}
+
+	public boolean setCodigo(int codigo) {
+		if (codigo < 0) {
+			return false;
+		}
+		return this.codigo = codigo;
+	}
+
+	public void getCodigo() {
+		return codigo;
+	}
+
+	public boolean setNome(String nome) {
+		if ("".equals(nome)) {
+			return false;
+		}
+		this.nome = nome;
+		return true;
+	}
+
+	public void getNome() {
+		return nome;
+	}
+
+	public void getDuracao() {
+		return duracao;
+	}
+
+	public boolean setDuracao(String duracao) {
+		if ("".equals(duracao)) {
+			return false;
+		}
+		this.duracao = duracao;
+		return true;
+	}
 }
--- src/models/Funcionario.java
+++ src/models/Funcionario.java
@@ -14,4 +14,72 @@ public class Funcionario {
 
     private float salario;
 
+    public boolean setNome(String nome) {
+        if ("".equals(nome)) {
+            return false;
+        }
+        this.codigo = codigo;
+        return true;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public boolean setNome(String nome) {
+        if ("".equals(nome)) {
+            return false;
+        }
+        this.nome = nome;
+        return true;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public boolean setTelefone(String telefone) {
+        if ("".equals(telefone)) {
+            return false;
+        }
+        this.telefone = telefone;
+        return true;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public boolean setCpf(String cpf) {
+        if ("".equals(cpf)) {
+            return false;
+        }
+        this.cpf= cpf;
+        return true;
+    }
+
+    public String getNumCTPS(String numCTPS) {
+        return numCTPS;
+    }
+
+    public boolean setNumCTPS(numCTPS) {
+        if ("".equals(numCTPS)) {
+            return false;
+        }
+        this.numCTPS = numCTPS;
+        return true;
+    }
+
+    public String getSalario() {
+        return salario;
+    }
+
+    public boolean setSalario(String salario) {
+        if (salario < 0) {
+            return false;
+        }
+
+        this.salario = salario;
+        return true;
+    }
 }
